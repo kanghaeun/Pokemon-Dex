fa8ad189b7c57997bd1736fc0554be1391a19a65
kanghaeun <xgkg0330@jnu.ac.kr>
2025-05-21 19:35:15 +0900

    feat: 포켓몬 리스트 및 선택 기능을 포괄하는 Dex페이지 구현

--- src/pages/Dex.jsx
+++ src/pages/Dex.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import styled from "styled-components";
+import Dashboard from "../components/Dashboard";
+import PokemonList from "../components/PokemonList";
 
 const Dex = () => {
-  return <div>Dex</div>;
+  return (
+    <DexLayput>
+      <Dashboard />
+      <PokemonList />
+    </DexLayput>
+  );
 };
 
 export default Dex;
+
+const DexLayput = styled.div`
+  background-color: #141420;
+  padding: 25px;
+`;
