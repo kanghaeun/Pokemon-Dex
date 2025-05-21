1eda9c5e68316813f1cc0fd6384c9603e4af090b
kanghaeun <xgkg0330@jnu.ac.kr>
2025-05-21 19:32:43 +0900

    chore: mock_data 생성

---
+++ src/data/MOCK_DATA.js
@@ -0,0 +1,265 @@
+export const pokemonData = [
+  {
+    id: 1,
+    name: "피카츄",
+    type: "전기",
+    description:
+      "전기를 모아 볼에 저장하고 있다가 방출합니다. 감정이 격해지면 방전이 일어납니다.",
+    image:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+  },
+  {
+    id: 2,
+    name: "리자몽",
+    type: "불꽃, 비행",
+    description: "입에서 내뿜는 불꽃은 바위를 녹일 정도로 뜨겁습니다.",
+    image:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/6.png",
+  },
+  {
+    id: 3,
+    name: "이상해꽃",
+    type: "풀, 독",
+    description:
+      "등의 꽃은 햇빛을 받으면 향기를 퍼뜨립니다. 기분 좋을 때는 꽃도 활짝 핍니다.",
+    image:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/3.png",
+  },
+  {
+    id: 4,
+    name: "거북왕",
+    type: "물",
+    description:
+      "등껍질에 달린 대포로 물을 고압으로 쏘아 강력한 공격을 합니다.",
+    image:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/9.png",
+  },
+  {
+    id: 5,
+    name: "뮤",
+    type: "에스퍼",
+    description:
+      "모든 포켓몬의 유전자를 가지고 있어 어떤 기술도 사용할 수 있습니다.",
+    image:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/151.png",
+  },
+  {
+    id: 6,
+    name: "뮤츠",
+    type: "에스퍼",
+    description:
+      "인간에 의해 인공적으로 만들어진 포켓몬으로 강력한 사이코 파워를 지녔습니다.",
+    image:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/150.png",
+  },
+  {
+    id: 7,
+    name: "나옹",
+    type: "노말",
+    description: "밤에 활동하며, 동전과 반짝이는 것을 매우 좋아합니다.",
+    image:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/52.png",
+  },
+  {
+    id: 8,
+    name: "팬텀",
+    type: "고스트, 독",
+    description:
+      "적의 그림자에 숨어서 기회를 엿보는 포켓몬. 등에 있는 가스는 독이 가득합니다.",
+    image:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/94.png",
+  },
+  {
+    id: 9,
+    name: "갸라도스",
+    type: "물, 비행",
+    description: "격분하면 도시도 파괴할 정도로 난폭한 성격을 가졌습니다.",
+    image:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/130.png",
+  },
+  {
+    id: 10,
+    name: "루카리오",
+    type: "격투, 강철",
+    description:
+      "상대의 기운을 읽고 전투에서 유리하게 작전을 펼칠 수 있습니다.",
+    image:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/448.png",
+  },
+  {
+    id: 11,
+    name: "에브이",
+    type: "에스퍼",
+    description: "해질녘에 활동하며, 고양이 같은 모습으로 인기 많습니다.",
+    image:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/196.png",
+  },
+  {
+    id: 12,
+    name: "블래키",
+    type: "악",
+    description:
+      "어두운 곳에서 적을 추적하며, 털이 빛나 상대를 혼란스럽게 합니다.",
+    image:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/197.png",
+  },
+  {
+    id: 13,
+    name: "닌피아",
+    type: "페어리",
+    description: "리본 같은 더듬이로 상대를 달래 싸움을 피하게 합니다.",
+    image:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/700.png",
+  },
+  {
+    id: 14,
+    name: "리피아",
+    type: "풀",
+    description:
+      "태양광을 이용해 광합성하며, 몸에서는 상쾌한 풀 내음이 납니다.",
+    image:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/470.png",
+  },
+  {
+    id: 15,
+    name: "글레이시아",
+    type: "얼음",
+    description: "주변의 수분을 얼려 얼음 결정으로 적을 공격합니다.",
+    image:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/471.png",
+  },
+  {
+    id: 16,
+    name: "부스터",
+    type: "불꽃",
+    description: "체온은 900도에 달하며 털을 부풀려 열을 내뿜습니다.",
+    image:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/136.png",
+  },
+  {
+    id: 17,
+    name: "샤미드",
+    type: "물",
+    description:
+      "물속에서 헤엄치는 데 최적화되어 있으며, 조용하고 차분한 성격입니다.",
+    image:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/134.png",
+  },
+  {
+    id: 18,
+    name: "쥬피썬더",
+    type: "전기",
+    description: "자신의 털을 이용해 정전기를 일으켜 공격합니다.",
+    image:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/135.png",
+  },
+  {
+    id: 19,
+    name: "에브이",
+    type: "노말",
+    description:
+      "환경에 따라 다양한 진화를 할 수 있는 유전자 구조를 지녔습니다.",
+    image:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/133.png",
+  },
+  {
+    id: 20,
+    name: "레쿠쟈",
+    type: "드래곤, 비행",
+    description: "대기권에서 떠돌며 지구의 평화를 지키는 전설의 포켓몬입니다.",
+    image:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/384.png",
+  },
+  {
+    id: 21,
+    name: "디아루가",
+    type: "강철, 드래곤",
+    description: "시간을 조종할 수 있으며, 전설 속의 신과 연결되어 있습니다.",
+    image:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/483.png",
+  },
+  {
+    id: 22,
+    name: "펄기아",
+    type: "물, 드래곤",
+    description:
+      "공간을 왜곡할 수 있는 능력을 가지고 있는 전설의 포켓몬입니다.",
+    image:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/484.png",
+  },
+  {
+    id: 23,
+    name: "레지기가스",
+    type: "노말",
+    description:
+      "레지 시리즈의 리더 격 존재로, 대륙을 끌고 이동할 정도로 힘이 셉니다.",
+    image:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/486.png",
+  },
+  {
+    id: 24,
+    name: "지우개",
+    type: "강철, 페어리",
+    description:
+      "강철처럼 단단한 몸과 동화 같은 외형을 지닌 신비로운 포켓몬입니다.",
+    image:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/809.png",
+  },
+  {
+    id: 25,
+    name: "자시안",
+    type: "페어리",
+    description:
+      "고대의 전설 속에서 용사를 도운 검을 물고 싸우는 늠름한 포켓몬입니다.",
+    image:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/888.png",
+  },
+  {
+    id: 26,
+    name: "자마젠타",
+    type: "격투",
+    description:
+      "방패 같은 털로 몸을 보호하며 정의를 수호하는 전설의 포켓몬입니다.",
+    image:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/889.png",
+  },
+  {
+    id: 27,
+    name: "나오하",
+    type: "풀",
+    description:
+      "풀잎 향기를 풍기며 주변을 즐겁게 합니다. 사람과 교감하는 데 능숙합니다.",
+    image:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/906.png",
+  },
+  {
+    id: 28,
+    name: "뚜따움",
+    type: "물",
+    description:
+      "화려한 깃털과 춤으로 상대를 현혹시키는 스타성 넘치는 포켓몬입니다.",
+    image:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/909.png",
+  },
+
+  {
+    id: 29,
+
+    name: "푸린",
+    type: "노말, 페어리",
+    description:
+      "노래로 상대를 잠들게 합니다. 감정에 따라 음높이가 달라집니다.",
+    image:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/39.png",
+  },
+
+  {
+    id: 30,
+    name: "잠만보",
+    type: "노말",
+    description:
+      "거의 하루 종일 잠을 자며, 깨어 있는 시간에는 먹는 데에만 집중합니다.",
+    image:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/143.png",
+  },
+];
