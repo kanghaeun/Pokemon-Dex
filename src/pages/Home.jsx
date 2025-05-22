d05e1fb27616f22beed2add4b318640626a6130c
kanghaeun <xgkg0330@jnu.ac.kr>
2025-05-23 08:45:58 +0900

    style: Home 스타일링

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
+import styled from "styled-components";
 import { useNavigate } from "react-router-dom";
-
+import logo from "../assets/logo.png";
 const Home = () => {
   const navigate = useNavigate();
 
@@ -8,10 +9,36 @@ const Home = () => {
     navigate("/dex");
   };
   return (
-    <div>
-      <button onClick={goToDex}>Play Now</button>
-    </div>
+    <HomeLayout>
+      <HomeLogo src={logo}></HomeLogo>
+      <MoveBtn onClick={goToDex}>Play Now</MoveBtn>
+    </HomeLayout>
   );
 };
 
 export default Home;
+
+const HomeLayout = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: #06080e;
+  height: 100vh;
+  gap: 30px;
+`;
+
+const HomeLogo = styled.img`
+  width: 250px;
+  height: auto;
+`;
+
+const MoveBtn = styled.div`
+  border: none;
+  font-size: 20px;
+  background-color: red;
+  padding: 15px;
+  border-radius: 20px;
+  color: white;
+  font-weight: ;
+`;
