5e06a1fe3ab752d5a06d588c073db6deb8e06f47
kanghaeun <xgkg0330@jnu.ac.kr>
2025-05-23 19:37:17 +0900

    feat: context API 적용

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,52 @@
+import React, { useState } from "react";
 import { Router } from "./router/Router.jsx";
+import { PokemonContext } from "./context/PokemonContext.js";
+import MOCK_DATA from "./data/mock.js";
+import toast, { Toaster } from "react-hot-toast";
 
 function App() {
+  const [pokemonCollection, setPokemonCollection] = useState([]);
+
+  const addToMyCollection = (id) => {
+    setPokemonCollection((prevCollection) => {
+      if (prevCollection.length >= 6) {
+        notify("더 이상 선택할 수 없습니다.");
+        return prevCollection;
+      }
+
+      const isDuplicate = prevCollection.some((pokemon) => pokemon.id === id);
+      if (isDuplicate) {
+        notify("이미 선택된 포켓몬입니다.");
+        return prevCollection;
+      }
+
+      const newCollection = MOCK_DATA.filter((pokemon) => id === pokemon.id);
+      return [...prevCollection, ...newCollection];
+    });
+
+    const notify = (message) => toast.error(message);
+  };
+
+  const deleteToMyCollection = (id) => {
+    const newCollection = pokemonCollection.filter(
+      (pokemon) => id !== pokemon.id
+    );
+    setPokemonCollection([...newCollection]);
+  };
+
   return (
-    <>
+    <PokemonContext.Provider
+      value={{
+        pokemonCollection,
+        addToMyCollection,
+        deleteToMyCollection,
+        MOCK_DATA,
+      }}
+    >
+      <Toaster position="top-center" reverseOrder={false} />
+
       <Router />
-    </>
+    </PokemonContext.Provider>
   );
 }
 
--- src/pages/Dex.jsx
+++ src/pages/Dex.jsx
@@ -2,48 +2,12 @@ import React, { useState } from "react";
 import styled from "styled-components";
 import Dashboard from "../components/Dashboard";
 import PokemonList from "../components/PokemonList";
-import MOCK_DATA from "../data/mock";
-import toast, { Toaster } from "react-hot-toast";
 
 const Dex = () => {
-  const notify = (message) => toast.error(message);
-
-  const [pokemonCollection, setPokemonCollection] = useState([]);
-
-  const addToMyCollection = (id) => {
-    setPokemonCollection((prevCollection) => {
-      if (prevCollection.length >= 6) {
-        notify("더 이상 선택할 수 없습니다.");
-        return prevCollection;
-      }
-
-      const isDuplicate = prevCollection.some((pokemon) => pokemon.id === id);
-      if (isDuplicate) {
-        notify("이미 선택된 포켓몬입니다.");
-        return prevCollection;
-      }
-
-      const newCollection = MOCK_DATA.filter((pokemon) => id === pokemon.id);
-      return [...prevCollection, ...newCollection];
-    });
-  };
-
-  const deleteToMyCollection = (id) => {
-    const newCollection = pokemonCollection.filter(
-      (pokemon) => id !== pokemon.id
-    );
-    setPokemonCollection([...newCollection]);
-  };
-
   return (
     <DexLayput>
-      <Toaster position="top-center" reverseOrder={false} />
-
-      <Dashboard
-        pokemonCollection={pokemonCollection}
-        deleteToMyCollection={deleteToMyCollection}
-      />
-      <PokemonList data={MOCK_DATA} addToMyCollection={addToMyCollection} />
+      <Dashboard />
+      <PokemonList />
     </DexLayput>
   );
 };
